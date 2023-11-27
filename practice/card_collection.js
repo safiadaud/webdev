8c68b215e88828d336d35c92b686b582594a89cb
Safia <safia.a.daud@gmail.com>
2023-11-27 14:58:05 +0000

    practice exercises

---
+++ practice/card_collection.js
@@ -0,0 +1,44 @@
+let count = 0;
+
+function cc(card) {
+
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count = count + 1;
+      break;  
+
+    case 7:
+    case 8:
+    case 9:
+      count = count + 0;
+      break;
+
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count = count - 1;
+      break;
+      
+      
+
+  }
+  if (count <= 0){
+    return count + " Hold"
+  }
+  else{
+    return count + " Bet"
+  }
+  // return "Change Me";
+  // Only change code above this line
+}
+
+
+
+cc(7); cc('K'); cc('A');
